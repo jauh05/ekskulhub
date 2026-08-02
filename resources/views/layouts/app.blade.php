daf9cb1d9c11ff13a67de824312f321b7cc82f10
jauh05 <jauharfua05@gmail.com>
2026-08-03 01:45:06 +0700

    Fix page title fallback and update favicon to graduation cap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Ekskul') }}</title>
+    <title>{{ config('app.name', 'EkskulHub') }}</title>
     <link rel="icon" type="image/svg+xml" href="/favicon.svg">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
