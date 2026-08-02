b42851fb6f2b00a5a9430606bdefcfba660e9fe7
jauh05 <jauharfua05@gmail.com>
2026-08-03 01:42:21 +0700

    Add school favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Ekskul') }}</title>
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         body {
